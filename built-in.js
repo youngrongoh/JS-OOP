db1e63c11b5be1c21256ab192e60957fac4be3f0
youngrongoh <youngrong.oh@gmail.com>
2020-07-02 18:51:12 +0900

    Lesson 4.2: 객체의 사용 사례 - 객체를 만들어  보기

--- built-in.js
+++ built-in.js
@@ -1,3 +1,24 @@
 console.log('Math.PI', Math.PI);
 console.log('Math.random()', Math.random());
 console.log('Math.floor(3,9)', Math.floor(3.9));
+
+const MyMath = {
+    PI: Math.PI,
+    random: function () {
+        return Math.random();
+    },
+    floor: function (value) {
+        return Math.floor(value);
+    }
+}
+console.log("MyMath.PI", MyMath.PI);
+console.log("MyMath.random", MyMath.random());
+console.log("MyMath.floor(3.9)", MyMath.floor(3.9));
+
+const MyMath_PI = Math.PI;
+function MyMath_random() {
+    return Math.random();
+}
+function MyMath_floor(value) {
+    return Math.floor(value);
+}
