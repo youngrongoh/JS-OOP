78f49f7ab2a75e0f508b709b831c82139a280b79
youngrongoh <youngrong.oh@gmail.com>
2020-07-02 20:01:44 +0900

    Lesson 6.3: constructor 만들기

--- object_factory.js
+++ object_factory.js
@@ -1,26 +1,26 @@
-const kim = {
-    name: '김',
-    first: 10,
-    second: 20,
-    third: 30,
-    sum: function () {
+function Person(name, first, second, third) {
+    this.name = name;
+    this.first = first;
+    this.second = second;
+    this.third = third;
+    this.sum = function () {
         return this.first + this.second + this.third;
     }
 }
 
-const lee = {
-    name: '이',
-    first: 10,
-    second: 10,
-    third: 10,
-    sum: function () {
-        return this.first + this.second + this.third;
-    }
-}
-
+const kim = new Person('kim', 10, 20, 30);
+const lee = new Person('lee', 10, 10, 10);
 console.log('kim.sum()', kim.sum());
 console.log('lee.sum()', lee.sum());
 
 const d1 = new Date('2019-4-10');
 console.log('d1.getFullYear()', d1.getFullYear());
 console.log('d1.getMonth()', d1.getMonth());
+
+console.log('Date', Date);
+
+
+
+console.log('Person()', Person());
+// constructor
+console.log('new Person()', new Person());
