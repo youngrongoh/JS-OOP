ac3463774c4e10052805776887cd4732d8b232d9
youngrongoh <youngrong.oh@gmail.com>
2020-07-06 21:35:35 +0900

    Lesson 10.1 메소드 구현

--- class.js
+++ class.js
@@ -1,28 +1,25 @@
-//9.1. class의 constructor function
-// class로 객체를 만들 때는 function이라는 키워드 없이 함수명을 바로 쓴다.
-// 객체가 생성될 때, 객체의 초기 상태를 정의하는 약속된 함수를 사용하는데, 바로 constructor이다.
+// 10.1 메소드 구현
+// Method: 객체에 소속된 함수
 
-
+// class 안에 함수를 넣으면 method로 동작하게 된다.
+// 이 때, function 키워는 쓰지 않는다!
 class Person {
     constructor(name, first, second) {
         this.name = name;
         this.first = first;
         this.second = second;
-        console.log('constructor');
+    }
+    sum() {
+        return 'prototype : ' + (this.first + this.second);
     }
 }
 
+
+// kim에서만 sum 메소드를 다르게 동작하도록 수정할 수 있다.
 const kim = new Person('kim', 10, 20);
-
-//호출한 적도 없는constructor가 먼저 찍히고 이어서 객체에 대한 console.log가 찍혔다.
-//이는 객체가 생성되는 과정에서 constructor 함수가 실행되었다는 것을 의미한다.
-console.log('kim', kim);
-
-//이렇게 객체가 생성될 때 초기값을 세팅하는 constructor를 클래스 내에서 구현하는 방법을 살펴보았다.
-
-// kim.sum = function () {
-//     return 'this : ' + (this.first + this.second);
-// }
-// const lee = new Person('lee', 10, 10); // kim의 sum 메소드만 수정되고 lee의 sum에는 영향이 없다.
-// console.log('kim.sum()', kim.sum());
-// console.log('lee.sum()', lee.sum());
+kim.sum = function () {
+    return 'this : ' + (this.first + this.second);
+}
+const lee = new Person('lee', 10, 10); // kim의 sum 메소드만 수정되고 lee의 sum에는 영향이 없다.
+console.log('kim.sum()', kim.sum());
+console.log('lee.sum()', lee.sum());
