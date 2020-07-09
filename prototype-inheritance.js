408d0f558a2894d6747c2cb3a70af0c8edbdb9fc
youngrongoh <youngrong.oh@gmail.com>
2020-07-09 21:53:49 +0900

    Lesson 13.4. 객체상속의 사용

--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -1,13 +1,8 @@
-// 13.3. Object.create()
-// __proto__의 대체제를 살펴보자.
-
 const superObj = { superVal: 'super' };
 
 // var subObj = { subVal: 'sub' };
 // subObj.__proto__ = superObj;
 
-// 어떤 객체를 부모로 하는 새로운 객체를 만드는 표준화된 방법은 Object.create()을 이용하는 것이다.
-// __proto__를 사용하는 것보다 Object.create()를 사용해 객체와 객체의 관계를 더 명확하게 지정해주는 것이 좋다.
 const subObj = Object.create(superObj);
 subObj.subVal = 'sub';
 debugger;
@@ -18,3 +13,34 @@ console.log('subObj.superVal =>', subObj.superVal);
 subObj.superVal = 'sub';
 console.log('subObj.superVal =>', subObj.superVal);
 console.log('superObj.superVal =>', superObj.superVal);
+
+// 13.4. 객체상속의 사용
+// __proto__와 표준인 Object.create()를 사용하여
+// 새로운 객체를 만들고 부모 객체에 없는 기능을 자식 객체에만 추가해보자.
+
+const kim = {
+    name: 'kim',
+    first: 10, second: 20,
+    sum: function () { return this.first + this.second }
+}
+// 1) Object.create()
+const lee = Object.create(kim);
+lee.name = 'lee';
+lee.first = 10;
+lee.second = 10;
+lee.avg = function () {
+    return (this.first + this.second) / 2;
+}
+
+// 2) __proto__ 사용
+// const lee = {
+//     name: 'lee',
+//     first: 10, second: 10,
+//     avg: function () {
+//         return (this.first + this.second) / 2;
+//     }
+// }
+// lee.__proto__ = kim;
+
+console.log('lee.sum():', lee.sum());
+console.log('lee.avg():', lee.avg());
