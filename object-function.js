bebd8880c2c3891351376e38afa77f0ecca78033
youngrongoh <youngrong.oh@gmail.com>
2020-07-10 09:24:12 +0900

    Lesson 14.3. bind

--- object-function.js
+++ object-function.js
@@ -1,26 +1,19 @@
-// 14.2.call
+// 14.3. bind
+// call 못지않게 독특한 bind라는 것이 있다.
+// bind는 묶는다 엮는다는 의미이다.
+// call은 호출될 때마다 내부의 this를 바꿔주었다면,
+// bind는 내부적으로 사용할 this를 영구적으로 고정시키는 것이다.
 const kim = { name: 'kim', first: 10, second: 20 };
 const lee = { name: 'lee', first: 10, second: 10 };
-function sum() {
-    return this.first + this.second;
-}
-// sum();
-sum.call(kim);
-// 모든 함수는 call이라고 하는 메소드를 가지고 있다.
-// JavaScript에서는 함수도 객체이기 때문이다.
-
-// sum은 kim이라는 객체에 속해있는 메소드가 아니었다.
-// 그런데, sum 안에 있는 call이라는 메소드를 통해
-// 마치 sum이 kim 안에 있는 메소드인 것 처럼 실행되었다.
-console.log("sum.call(kim) =>", sum.call(kim)); // sum.call(kim) => 30
-console.log("sum.call(lee) =>", sum.call(lee)); // sum.call(lee) => 20
-
-// call의 첫번째 인자로 그 함수의 내부적인 this를 무엇으로 할 것인가가 오고
-// 두번째 인자부터는 그 함수의 파라미터들이 오게 된다.
-function sum2(prefix) {
+function sum(prefix) {
     return prefix + (this.first + this.second);
 }
-console.log("sum2.call(kim)", sum2.call(kim, '=> ')); // sum2.call(kim) => 30
-console.log("sum2.call(lee)", sum2.call(lee, ': ')); // sum2.call(lee) : 20
 
-// call과 비슷하게 동작하는 apply라는 것도 있다.
+console.log("sum.call(kim)", sum.call(kim, '=> ')); // sum.call(kim) => 30
+console.log("sum.call(lee)", sum.call(lee, ': ')); // sum.call(lee) : 20
+
+const kimSum = sum.bind(kim, '-> ');
+console.log('kimSum()', kimSum()); // kimSum() -> 30
+
+// call은 새로운 함수로 할당할 수 없다.
+// const leeSum = sum.call(lee, '->');
